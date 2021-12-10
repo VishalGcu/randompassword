47d61754d3ff34fbf4a5ea134b07413bec13568c
VishalGcu <83893470+VishalGcu@users.noreply.github.com>
2021-12-10 13:45:52 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import random
 s = int(input())
+#cha here holds the different charector, which will be used to make a password
 cha = 'abcdefghijklmnopqrstuvwxyz/"''".{}()[]'
+#sample here takes 2 variable
 get_pass= random.sample(cha,s)
 password = ''
 for i in range(len(get_pass)):
